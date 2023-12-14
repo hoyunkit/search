851e120a88786e94575c4eb85c9fde2e247a9335
rickie-austin-114 <rcya996001@gmail.com>
2023-12-15 06:11:04 +0800

    Last edit by Rickie before he sleep

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -6,7 +6,17 @@ const Map = () => {
 
     return (
         <div>
-            
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+            <gmp-map id="marker-click-event-example" center="22.3193, 114.1694" zoom="15" map-id="DEMO_MAP_ID">
+                <gmp-advanced-marker position="22.3193, 114.1694" title="MK"></gmp-advanced-marker>
+                <gmp-advanced-marker position="22.3193, 114.1894" title="ToGuaWan"></gmp-advanced-marker>
+            </gmp-map>
+
+            <script
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBedbw_-j59ihSLX0n4GEezzV0e4bt3IPY&callback=initMap&libraries=marker&v=beta"
+                defer></script>
+                
             <ListOfEvents />
             <Comments />
         </div>
